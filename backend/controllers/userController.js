3af0e77ee29c9fed34d975c287f72fe6ac310ada
Jigar Sable <64949957+jigar-sable@users.noreply.github.com>
2022-03-21 18:14:39 +0530

    feat: add user details endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ const User = require('../models/userModel');
 const ErrorHandler = require('../utils/errorHandler');
 const sendCookie = require('../utils/sendCookie');
 
+// Register User
 exports.registerUser = catchAsync(async (req, res, next) => {
 
     const { name, email, password } = req.body;
@@ -16,6 +17,7 @@ exports.registerUser = catchAsync(async (req, res, next) => {
     sendCookie(user, 201, res);
 });
 
+// Login User
 exports.loginUser = catchAsync(async (req, res, next) => {
 
     const { email, password } = req.body;
@@ -45,5 +47,16 @@ exports.logoutUser = catchAsync(async (req, res, next) => {
     res.status(200).json({
         success: true,
         message: "Logged Out",
+    });
+});
+
+// Get User Details --Logged In User
+exports.getAccountDetails = catchAsync(async (req, res, next) => {
+
+    const user = await User.findById(req.user._id);
+
+    res.status(200).json({
+        success: true,
+        user,
     });
 });
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { registerUser, loginUser, logoutUser } = require('../controllers/userController');
+const { registerUser, loginUser, logoutUser, getAccountDetails } = require('../controllers/userController');
+const { isAuthenticated } = require('../middlewares/auth');
 
 const router = express();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logoutUser);
+router.route("/me").get(isAuthenticated, getAccountDetails);
 
 module.exports = router;
