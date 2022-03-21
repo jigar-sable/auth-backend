64ee8ee59ed158e5a81afcc07a319222167b54bb
Jigar Sable <64949957+jigar-sable@users.noreply.github.com>
2022-03-21 18:08:53 +0530

    feat: add logout endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -34,3 +34,16 @@ exports.loginUser = catchAsync(async (req, res, next) => {
 
     sendCookie(user, 200, res);
 });
+
+// Logout User
+exports.logoutUser = catchAsync(async (req, res, next) => {
+    res.cookie('token', null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Logged Out",
+    });
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userController');
+const { registerUser, loginUser, logoutUser } = require('../controllers/userController');
 
 const router = express();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/logout").get(logoutUser);
 
 module.exports = router;
